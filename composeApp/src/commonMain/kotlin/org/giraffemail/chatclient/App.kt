cf2568e6491764daf990c8247d6e22a3198f0938
pydlv <pydlv64@gmail.com>
2024-09-17 22:13:08 -0700

    Switch from Drawer to NavigationRail
    Replaced the ModalDrawer with a NavigationRail component for better navigation. Updated UI components to display icons and labels within the NavigationRail, categorizing items into top and bottom options. Removed unused imports and preview annotation to clean up the code.

--- composeApp/src/commonMain/kotlin/org/giraffemail/chatclient/App.kt
+++ composeApp/src/commonMain/kotlin/org/giraffemail/chatclient/App.kt
@@ -1,73 +1,89 @@
 package org.giraffemail.chatclient
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.NavigationRail
+import androidx.compose.material.NavigationRailItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.sharp.ArrowBack
+import androidx.compose.material.icons.sharp.Edit
+import androidx.compose.material.icons.sharp.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
 import org.giraffemail.chatclient.screens.SettingsScreen
 import org.giraffemail.chatclient.screens.TopLevelChatScreen
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 enum class Route(val route: String) {
     Chats("chats"),
     Settings("settings")
 }
 
-data class DrawerOption(val text: String, val route: Route);
+data class MenuItem(val text: String, val route: Route, val icon: ImageVector);
 
-val options: List<DrawerOption> = listOf(
-    DrawerOption("Chats", Route.Chats),
-    DrawerOption("Settings", Route.Settings),
+val topOptions: List<MenuItem> = listOf(
+    MenuItem("Back", Route.Chats, Icons.AutoMirrored.Sharp.ArrowBack),
+    MenuItem("Chats", Route.Chats, Icons.Sharp.Edit),
+)
+
+val bottomOptions: List<MenuItem> = listOf(
+    MenuItem("Settings", Route.Settings, Icons.Sharp.Settings),
 )
 
 @Composable
-@Preview
 fun App() {
     val navController = rememberNavController()
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
 
     MaterialTheme {
-        ModalDrawer(
-            drawerState = drawerState,
-            drawerContent = {
-            Text(
-                text = "ChatClient",
-                color = MaterialTheme.colors.onSurface,
-                style = MaterialTheme.typography.h5,
-                modifier = Modifier.padding(8.dp)
-            )
-            Divider()
-            options.forEach { option ->
-                val isSelected = navBackStackEntry?.destination?.route == option.route.route
-                TextButton(
-                    colors = ButtonDefaults.textButtonColors(
-                        contentColor = if (isSelected) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface,
-                        backgroundColor = if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.surface
-                    ),
-                    modifier = Modifier.fillMaxWidth(),
-                    content = { Text(option.text) },
-                    onClick = {
-                        navController.navigate(option.route.route)
-                        scope.launch { drawerState.close() }
+        Row {
+            NavigationRail {
+                Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxHeight()) {
+                    Column {
+                        topOptions.forEach { option ->
+                            val isSelected = navBackStackEntry?.destination?.route == option.route.route
+                            NavigationRailItem(
+                                selected = isSelected,
+                                label = { option.text },
+                                icon = { Icon(option.icon, option.text) },
+                                onClick = {
+                                    navController.navigate(option.route.route)
+                                }
+                            )
+                        }
+
                     }
-                )
+
+                    Column {
+                        bottomOptions.forEach { option ->
+                            val isSelected = navBackStackEntry?.destination?.route == option.route.route
+                            NavigationRailItem(
+                                selected = isSelected,
+                                label = { option.text },
+                                icon = { Icon(option.icon, option.text) },
+                                onClick = {
+                                    navController.navigate(option.route.route)
+                                }
+                            )
+                        }
+                    }
+                }
             }
-            }
-        ) {
-            NavHost(navController, startDestination = Route.Chats.route) {
-                composable(route = Route.Chats.route) { TopLevelChatScreen() }
-                composable(route=Route.Settings.route) { SettingsScreen() }
+
+            Column(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                NavHost(navController, startDestination = Route.Chats.route) {
+                    composable(route = Route.Chats.route) { TopLevelChatScreen() }
+                    composable(route = Route.Settings.route) { SettingsScreen() }
+                }
             }
         }
     }
