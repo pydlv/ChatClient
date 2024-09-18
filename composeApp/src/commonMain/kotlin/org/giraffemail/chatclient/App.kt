9756e57c05a1d8191e514d8ec2815760be7e4aff
pydlv <pydlv64@gmail.com>
2024-09-17 19:02:57 -0700

    Refactor drawer navigation UI in App.kt
    Simplify drawer navigation by using a list of DrawerOption objects. Enhance the drawer UI with styled text and text buttons for navigation items, improving overall usability and appearance.

--- composeApp/src/commonMain/kotlin/org/giraffemail/chatclient/App.kt
+++ composeApp/src/commonMain/kotlin/org/giraffemail/chatclient/App.kt
@@ -1,8 +1,12 @@
 package org.giraffemail.chatclient
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -16,6 +20,13 @@ enum class Route(val route: String) {
     Settings("settings")
 }
 
+data class DrawerOption(val text: String, val route: Route);
+
+val options: List<DrawerOption> = listOf(
+    DrawerOption("Chats", Route.Chats),
+    DrawerOption("Settings", Route.Settings),
+)
+
 @Composable
 @Preview
 fun App() {
@@ -24,24 +35,27 @@ fun App() {
 
     MaterialTheme {
         ModalDrawer(drawerContent = {
-            Text("ChatClient")
+            Text(
+                text = "ChatClient",
+                color = MaterialTheme.colors.onSurface,
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier.padding(8.dp)
+            )
             Divider()
-            NavigationRailItem(
-                label= { Text("Chats") },
-                icon={},
-                selected=navBackStackEntry?.destination?.route == Route.Chats.route,
-                onClick = {
-                    navController.navigate(Route.Chats.route)
-                }
-            )
-            NavigationRailItem(
-                label= { Text("Settings") },
-                icon={},
-                selected=navBackStackEntry?.destination?.route == Route.Settings.route,
-                onClick = {
-                    navController.navigate(Route.Settings.route)
-                }
-            )
+            options.forEach { option ->
+                val isSelected = navBackStackEntry?.destination?.route == option.route.route
+                TextButton(
+                    colors = ButtonDefaults.textButtonColors(
+                        contentColor = if (isSelected) MaterialTheme.colors.onPrimary else MaterialTheme.colors.onSurface,
+                        backgroundColor = if (isSelected) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+                    ),
+                    modifier = Modifier.fillMaxWidth(),
+                    content = { Text(option.text) },
+                    onClick = {
+                        navController.navigate(option.route.route)
+                    }
+                )
+            }
         }) {
             NavHost(navController, startDestination = Route.Chats.route) {
                 composable(route=Route.Chats.route) { ChatListScreen() }
