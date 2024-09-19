fb4fcde042147bc4169d2512a30ab1afad7df8d3
pydlv <pydlv64@gmail.com>
2024-09-18 21:11:23 -0700

    Add icon tinting based on the message sender
    Updated the ChatInstance screen to tint icons gray for user messages and blue for AI messages. This enhances the visual distinction between different types of senders, improving user experience and message readability.

--- composeApp/src/commonMain/kotlin/org/giraffemail/chatclient/screens/ChatInstance.kt
+++ composeApp/src/commonMain/kotlin/org/giraffemail/chatclient/screens/ChatInstance.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 enum class Sender {
@@ -101,7 +102,8 @@ fun MessageRow(message: Message) {
                 imageVector =
                 if (message.sender == Sender.User) Icons.Sharp.Person
                 else RobotIcon,
-                contentDescription = if (message.sender == Sender.User) "User" else "AI"
+                contentDescription = if (message.sender == Sender.User) "User" else "AI",
+                tint = if (message.sender == Sender.User) Color.Gray else Color.Blue,
             )
         }
         Text(message.content)
